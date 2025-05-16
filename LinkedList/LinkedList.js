fcb1751dc1523953f4ca6d5a48dac2fbaa7821dc
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-05-15 22:43:13 -0300

    Linked List Push Method

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -1,17 +1,30 @@
 class Node {
-    constructor(value){
-        this.head = value
-        this.next = null
-    }
+  constructor(value) {
+    this.head = value;
+    this.next = null;
+  }
 }
 
 class LinkedList {
-    constructor(value) {
-        this.value = new Node(value)
-        this.tail  = this.head
-        this.lenght = 1
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.lenght = 1;
+  }
+
+  // Linked List Push Method
+  push(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
     }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.lenght++;
+  }
 }
 
-const myLinkedList = new LinkedList(1)
-console.log(myLinkedList)
+const myLinkedList = new LinkedList(1);
+myLinkedList.push(10);
+console.log(myLinkedList);
