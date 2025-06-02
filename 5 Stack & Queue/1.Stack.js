d44c670409a6fc016594a87adc09e2a12b90cc28
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-06-02 20:41:02 -0300

    Stack Push Method

--- 5 Stack & Queue/1.Stack.js
+++ 5 Stack & Queue/1.Stack.js
@@ -8,11 +8,27 @@ class Node {
 class Stack{
     constructor(value) {
         const newNode = new Node(value)
-        this.fist = newNode
+        this.first = newNode
         this.length = 1 
+    }
+
+    //Stack Push Method
+    push(value) {
+        const newNode = new Node(value)
+
+        if(this.length === 0){
+            this.first = newNode
+        }
+
+        // o proximo adicionodo agora e o primeiro
+        newNode.next = this.first
+        this.first = newNode
+        this.length++
+        return this
     }
 }
 
 let theStack = new Stack(0)
-
+theStack.push(1)
+theStack.push(2)
 console.log(theStack)
