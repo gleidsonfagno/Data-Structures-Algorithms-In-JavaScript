53b5d1350598f90a940d62d9312dddfc7962d15f
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-06-09 21:59:29 -0300

    Queues Enqueue Method

--- 5 Stack & Queue/1.Stack.js
+++ 5 Stack & Queue/1.Stack.js
@@ -1,34 +1,50 @@
 class Node {
-    constructor(value) {
-        this.value = value
-        this.next = null
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-class Stack{
-    constructor(value) {
-        const newNode = new Node(value)
-        this.first = newNode
-        this.length = 1 
+class Stack {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.first = newNode;
+    this.length = 1;
+  }
+
+  //Stack Push Method
+  push(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.first = newNode;
     }
 
-    //Stack Push Method
-    push(value) {
-        const newNode = new Node(value)
+    // o proximo adicionodo agora e o primeiro
+    newNode.next = this.first;
+    this.first = newNode;
+    this.length++;
+    return this;
+  }
 
-        if(this.length === 0){
-            this.first = newNode
-        }
-
-        // o proximo adicionodo agora e o primeiro
-        newNode.next = this.first
-        this.first = newNode
-        this.length++
-        return this
+  // Stack Push Method
+  pop() {
+    if (this.length === 0) {
+      return undefined;
     }
+
+    // guardamos o prrimeiro elemento em uma variavel temp e apontamos para o proximo
+    let temp = this.first;
+    this.first = this.first.next;
+    temp.next = null;
+    this.length--;
+    return temp;
+  }
+
 }
 
-let theStack = new Stack(0)
-theStack.push(1)
-theStack.push(2)
-console.log(theStack)
+let theStack = new Stack(0);
+theStack.push(1);
+theStack.push(2);
+theStack.pop();
+console.log(theStack);
---
+++ 5 Stack & Queue/2.Queue.js
@@ -0,0 +1,35 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.first = newNode;
+    this.last = newNode;
+    this.length = 1;
+  }
+
+  //   Queues Enqueue Method
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+        this.first = newNode
+        this.last = newNode
+    }
+
+    this.last.next = newNode
+    this.last =  newNode
+    this.length++
+    return this
+  }
+
+}
+let myQueue = new Queue(0);
+myQueue.enqueue(1)
+myQueue.enqueue(2)
+console.log(myQueue);
