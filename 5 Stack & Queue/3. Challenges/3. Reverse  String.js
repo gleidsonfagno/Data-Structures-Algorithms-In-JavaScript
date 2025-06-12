5b88b082e7d732f057e115ff6a58f20e58228172
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-06-12 20:17:04 -0300

    Challenges: Reverse String Using Stack

---
+++ 5 Stack & Queue/3. Challenges/3. Reverse  String.js
@@ -0,0 +1,19 @@
+function reverseString(str) {
+  const stack = [];
+
+  for (let char of str) {
+    stack.push(char);
+  }
+
+  let reversedStr = "";
+
+  while (stack.length > 0) {
+    reversedStr += stack.pop();
+  }
+
+  return reversedStr
+}
+
+const reversedString = reverseString("Hello world");
+
+console.log(reversedString);
