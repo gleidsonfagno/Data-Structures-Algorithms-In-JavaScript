62f1a1d71a3678ca1e818eebfab46e44b833c3c3
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-06-14 11:16:55 -0300

    Hash Table Set Method

--- 6 Hash Tables/1.Custom Hash Table/index.js
+++ 6 Hash Tables/1.Custom Hash Table/index.js
@@ -7,11 +7,22 @@ class HashTable {
     let sum = 0;
     const PRIMARY_NUMBER = 31;
 
-    for (let i = 0; i < Math.min(key.lenght, 100); i++) {
-      const charCode = key.charCode(0) - 96;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      const charCode = key.charCodeAt(0) - 96;
       sum = (sum * PRIMARY_NUMBER + charCode) % this.keyMap.length;
     }
 
-    return sum
+    return sum;
+  }
+
+  set(key, value) {
+    const index = this._hashFunctio(key);
+    if(!this.keyMap[index]) this.keyMap[index] = [];
+    this.keyMap[index].push([key, value])
+    return this;
   }
 }
+
+const phoneBook = new HashTable();
+
+console.log(phoneBook.set("Jhon", "333-444-222"))
