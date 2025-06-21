698c12b5d49fa6a6e7c9ce7abf528752efec64e7
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-06-21 14:20:49 -0300

    Creating Our First Binary Search Tree

---
+++ 7 Tree/1. BinarySearchTree/index.js
@@ -0,0 +1,21 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+}
+const tree = new BinarySearchTree()
+
+console.log(tree)
+
+// Creating Our First Binary Search Tree
+// 4:02:15 - Binary Search Tree Insert Method
+// 4:08:36 - Binary Search Tree Includes Method
