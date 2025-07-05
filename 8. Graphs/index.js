03fde1c4299acbed10ca702a5b6b4473fa61027b
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-07-05 07:52:17 -0300

    Add Vertex To Graph

--- 8. Graphs/index.js
+++ 8. Graphs/index.js
@@ -3,8 +3,22 @@ class Graph {
     this.adjacencyList = {};
   }
 
+  addVertex(vtx) {
+    if (!this.adjacencyList[vtx]) {
+      this.adjacencyList[vtx] = [];
+      return true;
+    }
+
+    return false;
+  }
   
 }
 
 const g = new Graph();
+
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+
+
 console.log(g);
