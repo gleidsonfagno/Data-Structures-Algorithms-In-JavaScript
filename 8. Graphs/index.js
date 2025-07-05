17725c0bfa726bc2b573ac7cca85f263ff11a871
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-07-05 08:00:31 -0300

    Add Edges To Graph

--- 8. Graphs/index.js
+++ 8. Graphs/index.js
@@ -11,6 +11,16 @@ class Graph {
 
     return false;
   }
+
+  // connection
+  addEge(vtx1, vtx2) {
+    if(this.adjacencyList[vtx1] && this.adjacencyList[vtx2]) {
+        this.adjacencyList[vtx1].push(vtx2)
+        this.adjacencyList[vtx2].push(vtx1)
+        return true
+    }
+    return false
+  }
   
 }
 
@@ -20,5 +30,7 @@ g.addVertex("A");
 g.addVertex("B");
 g.addVertex("C");
 
+g.addEge("A", "B")
 
+// 4:58:36 - 
 console.log(g);
